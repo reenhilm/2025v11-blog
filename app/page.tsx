79f71c3e2a947c745bd96bf9ada4c5127891befd
salight <light.sara@icloud.com>
2025-03-18 14:57:00 +0100

    updated Contacts with its own contacs/page.tsx yet again

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import SearchResultList from "./search-result-list";
 import { fetchTopViewedPosts } from "./actions";
-import Contacts from "./components/Contacts";
 
 export default async function Home() {
   const posts = fetchTopViewedPosts(5);
   
   return (
     <main className="flex flex-col m-auto items-center max-w-120">
-    <Contacts />   
       <section className="flex my-4 border-1 rounded">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Search</button>
         <input className="indent-4" type="text" name="query" aria-labelledby="Search Query" id="query" required placeholder="Search for..." />
