dfca3703cff365779592f4232bf03a51dc9c4e4d
Christian Rönnholm <christian@kajal.se>
2025-03-19 11:18:30 +0100

    #20 Startpage - added clickeventhandler that re-routes to search. Changed searchsection into a form

---
+++ app/components/search-input-section.tsx
@@ -0,0 +1,37 @@
+"use client"
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function SearchInputSection() {
+    const [query, setQuery] = useState("");
+    const router = useRouter();
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (query.trim()) {
+            router.push(`/search?query=${encodeURIComponent(query)}`);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="flex my-4 border-1 rounded">
+            <input
+                className="indent-4 px-2 py-1"
+                type="text"
+                name="query"
+                aria-labelledby="Search Query"
+                id="query"
+                required
+                placeholder="Search for..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
+            <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+                Search
+            </button>
+        </form>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 import SearchResultList from "./search-result-list";
 import { fetchTopViewedPosts } from "./actions";
+import SearchInputSection from "./components/search-input-section";
 
 export default async function Home() {
   const posts = fetchTopViewedPosts(5);
 
   return (
     <main className="flex flex-col m-auto items-center max-w-120">
-      <section className="flex my-4 border-1 rounded">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Search</button>
-        <input className="indent-4" type="text" name="query" aria-labelledby="Search Query" id="query" required placeholder="Search for..." />
-      </section>
+      <SearchInputSection />
       <h1 className="text-3xl">Most viewed posts</h1>
       <SearchResultList posts={posts} />
     </main>
