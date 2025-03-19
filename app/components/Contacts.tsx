fc221daae71676ff854afd13e87f42cdeff7e15f
salight <light.sara@icloud.com>
2025-03-19 11:22:15 +0100

    15-contact-page (#26)
    * Added basic Contact info with its own contacs/page.tsx
    
    * minor change

---
+++ app/components/Contacts.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const contacts = 
+  [  
+  { id: 1, name: 'Aemail', role: 'CEO', email: 'aemail@gmail.com'},
+  { id: 2, name: 'Bemail', role: 'CTO', email: 'bemail@gmail.com'},
+  { id: 3, name: 'Cemail', role: 'COO', email: 'cemail@gmail.com'},  
+  ];
+
+const Contacts: React.FC = () => {
+    return (
+      <div>
+        <h1>For questions about this beauteous web page, please do not hesitate to contact:</h1>
+        <ul className="flex space-x-4">
+          {contacts.map(contact => (
+            <li key={contact.id}>
+              <strong>{contact.name}</strong>: {contact.email}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+  
+  export default Contacts;
---
+++ app/contacts/page.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import Contacts from '../components/Contacts';
+
+export default function ContactsPage() {
+  return (
+    <main>
+        <section className="max-w-lg mx-auto p-4 mt-5">
+            <Contacts  />
+        </section>
+    </main>
+  )
+}
