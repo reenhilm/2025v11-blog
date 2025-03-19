9d84c025761911990da3bea72f39b3703036270e
Christian Rönnholm <christian@kajal.se>
2025-03-19 13:48:43 +0100

    Merged master into origin/#20

--- app/page.tsx
+++ app/page.tsx
@@ -2,29 +2,24 @@ import SearchResultList from "./search-result-list";
 import { fetchTopViewedPosts } from "./actions";
 import { ModeToggle } from "./components/mode-toggle";
 import { ThemeProvider } from "./components/theme-provider";
+import SearchInputSection from "./components/search-input-section";
 
 export default async function Home() {
   const posts = fetchTopViewedPosts(5);
 
   return (
-    <main className="flex flex-col m-auto items-center max-w-120 theme-set-values_ordinary">
-      <div className="grid grid-cols-(--layout-grid-cols) theme-set-values_ordinary">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-          >     
+    <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+    >
+      <main className="flex flex-col m-auto items-center max-w-120 theme-set-values_ordinary">
         <ModeToggle />
-        <section className="flex my-4 border-1 rounded 
-        theme-set-values_ordinary">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded theme-set_search-buton">Search</button>
-          <input className="indent-4 theme-set-values_ordinary theme-set-values_ordinary" type="text" name="query" aria-labelledby="Search Query" id="query" required placeholder="Search for..." />
-        </section>
-        <h1 className="text-3xl theme-set-values_ordinary">Most viewed posts</h1>
+        <SearchInputSection />
+      <h1 className="text-3xl theme-set-values_ordinary">Most viewed posts</h1>
         <SearchResultList posts={posts} />
-        </ThemeProvider> 
-      </div>
-    </main>
+      </main>
+    </ThemeProvider>
   )
 }
