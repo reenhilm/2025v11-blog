32a5578f71bebe8155bf1308904944969b623bb3
Mohamed-Albushra <mohamedalbushra39@gmail.com>
2025-03-19 15:51:14 +0100

    #2 url problem (#35)
    * #2 fixing the searching URL by adding useSearchParams
    
    * Merged master into origin/#2
    
    * #2 fixing the search and refactor the interface
    
    * bugfix: if empty search-query, show error
    
    ---------
    
    Co-authored-by: Christian Rönnholm <christian@kajal.se>

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import Searching from '../components/Searching';
 
 export default function Search() {
   return (
     <main>
-        <section className="max-w-lg mx-auto p-4 mt-5">
-            <Searching  />
+      <section className="max-w-lg mx-auto p-4 mt-5">
+          <Suspense fallback={<div>Loading...</div>}>
+            <Searching />
+          </Suspense>
         </section>
     </main>
   )
