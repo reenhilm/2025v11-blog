9d95ac2147f7aced7d013aa71afe892f69224b09
salight <light.sara@icloud.com>
2025-03-19 10:07:32 +0100

    reverting

--- app/components/Contacts.tsx
+++ app/components/Contacts.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const contacts: Contact[] = [
-  
+const contacts = 
+  [  
   { id: 1, name: 'Aemail', role: 'CEO', email: 'aemail@gmail.com'},
   { id: 2, name: 'Bemail', role: 'CTO', email: 'bemail@gmail.com'},
   { id: 3, name: 'Cemail', role: 'COO', email: 'cemail@gmail.com'},  
   ];
 
-  const Contacts: React.FC = () => {
+const Contacts: React.FC = () => {
     return (
       <div>
         <h1>For questions about this beauteous web page, please do not hesitate to contact:</h1>
