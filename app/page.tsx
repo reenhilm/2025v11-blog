354cf941fd72716e605b2aa5c486a5690c69bcac
Sondaicus <54620152+Sondaicus@users.noreply.github.com>
2025-03-24 10:25:46 +0100

    Try button again (#52)
    * Added gitignor stuff
    
    * Finished retries
    
    * Tog bort konfilkterande css kod

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,15 @@
 import SearchResultList from "./search-result-list";
 import { fetchTopViewedPosts } from "./actions";
-import { ModeToggle } from "./components/mode-toggle";
-import { ThemeProvider } from "./components/theme-provider";
 import SearchInputSection from "./components/search-input-section";
 
 export default async function Home() {
   const posts = fetchTopViewedPosts(5);
 
   return (
-    <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-    >
       <main className="flex flex-col m-auto items-center max-w-120 theme-set-values_ordinary">
-        <ModeToggle />
         <SearchInputSection />
       <h1 className="text-3xl theme-set-values_ordinary">Most viewed posts</h1>
         <SearchResultList posts={posts} />
       </main>
-    </ThemeProvider>
   )
 }
