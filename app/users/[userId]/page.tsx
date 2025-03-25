49d1ca7165af19d9e2fae8a3eda208a1601f489e
Christian Rönnholm <christian@kajal.se>
2025-03-25 12:45:26 +0100

    fetching both calls in parallel

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -1,38 +1,30 @@
 "use server"
 import { fetchPostsByUserId, fetchUserById } from "@/app/actions";
 import UserPostsResult from "./user-posts-result";
-import { Post } from "@/interfaces/posts";
-import { User } from "@/interfaces/user";
 import { notFound } from "next/navigation";
 import ApiError from "@/classes/api-error";
 import ErrorDialog from "@/app/components/error-dialog";
 
 export default async function PostPage({ params }: { params: Promise<{ userId: number }> }) {
     const { userId } = await params;
-    const postsResult: Post[] | ApiError = await fetchPostsByUserId(userId);
-    const userResult: User | ApiError = await fetchUserById(userId);
+
+    //Fetch both API calls in parallel
+    const [userResult, postsResult] = await Promise.all([
+        fetchUserById(userId),
+        fetchPostsByUserId(userId),
+    ]);
 
     if (userResult instanceof ApiError) {
         if (userResult.show404) {
             return notFound();
-        } else if (userResult.showCustom) {
-            return (
-                <ErrorDialog
-                    message={userResult.message}
-                />
-            )
         }
-    } else if (postsResult instanceof ApiError) {
-        if (postsResult.showCustom) {
-            return (
-                <ErrorDialog
-                    message={postsResult.message}
-                />
-            )
-        }
-    } else {    
-        return (
-            <UserPostsResult posts={postsResult} user={userResult} />
-            );
+        return <ErrorDialog message={userResult.message} />;
     }
+
+    if (postsResult instanceof ApiError) {
+        return <ErrorDialog message={postsResult.message} />;
+    }
+
+    //Both calls succeeded
+    return <UserPostsResult posts={postsResult} user={userResult} />;
 }
