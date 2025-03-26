8a231da05ab7b76743ddb4bbbf50fefc84a36e60
Christian Rönnholm <christian@kajal.se>
2025-03-26 11:27:31 +0100

    feat: added items-center to container-element of navbar-links (#64)
    Looks good.

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "./theme-provider";
 const Navbar: React.FC = () => {
     return (
       <nav className="bg-blue-500 text-white p-4 theme-set_redirect-element-field">
-        <ul className="flex space-x-4">
+        <ul className="flex items-center space-x-4">
           <li>
             <Link href="/" className="hover:underline">Home</Link>
           </li>
